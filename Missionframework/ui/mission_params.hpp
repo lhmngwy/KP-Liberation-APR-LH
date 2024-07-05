1d4447fd012e7357b3980783717fc00e2d9d66f4
lhmngwy <1087611+lhmngwy@users.noreply.github.com>
2024-07-06 00:28:53 +0100

    Disable respawn cooldown

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -183,7 +183,7 @@ class Params {
         title = $STR_PARAM_RESPAWN_COOLDOWN;
         values[] = {0, 300, 600, 900, 1200, 1800, 3600};
         texts[] = {$STR_PARAMS_DISABLED, 5, 10, 15, 20, 30, 60};
-        default = 300;
+        default = 0;
     };
     class MobileArsenal {
         title = $STR_PARAMS_MOBILEARSENAL;
