86d6f8600f26196f9e60d01dc0223b0293683f3b
lhmngwy <1087611+lhmngwy@users.noreply.github.com>
2024-08-09 16:24:35 +0100

    Increase mobile respawn cooldown

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -183,7 +183,7 @@ class Params {
         title = $STR_PARAM_RESPAWN_COOLDOWN;
         values[] = {0, 300, 600, 900, 1200, 1800, 3600};
         texts[] = {$STR_PARAMS_DISABLED, 5, 10, 15, 20, 30, 60};
-        default = 0;
+        default = 600;
     };
     class MobileArsenal {
         title = $STR_PARAMS_MOBILEARSENAL;
